85341a6b49137c0129c221f832574697c33df7e8
Muhammed Faizan <156081595+FaizanTahir450@users.noreply.github.com>
2026-03-06 12:23:06 +0500

    Delete old backend-python directory
    delete old backend of python not having email and 2 more functionality it will be replaced by new backend python fully structured and functional

--- backend-python/run.py
+++
@@ -1,9 +0,0 @@
-# run.py
-import uvicorn
-
-if __name__ == "__main__":
-    # print("Starting Customer Support API...")
-    # print("API will be available at http://localhost:8000")
-    # print("Docs available at http://localhost:8000/docs")
-    # print("="*50)
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
